44e96e981c51ee6341946f3b726049832b6d9a58
Alex <Alex@DESKTOP-GNQ50LS>
2022-05-21 19:57:48 +0700

    Добавьте файлы проекта.

---
+++ Vector_project/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Vector_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector = new Vector(11, 22, 33);
+            Vector vector1 = new Vector(44, 33, 66);
+
+            Console.WriteLine($"Длина вектора: {vector.GetLenght()}");
+            Console.WriteLine($"Скалярное произведение: { vector.ScalarSumm(vector, vector1)}");
+            Console.WriteLine($"Векторное произведение: {vector.CrossProduct(vector,vector1)}");
+            Console.WriteLine($"Угол между векторами: {vector.GetCos(vector,vector1)}");
+            Console.WriteLine($"Сумма векторов: {vector.GetSumm(vector,vector1)}");
+            Console.WriteLine($"разность векторов: {vector.GetMinus(vector, vector1)}");         
+        }
+
+    }
+}
+
+
+
+
+
+
+    
+   
+
+
+
---
+++ Vector_project/Vector.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_project
+{
+    class Vector
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        int Z { get; set; }
+
+       public Vector() { 
+        }
+        public Vector(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public double GetLenght()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+
+        public double ScalarSumm(Vector vector, Vector vector1)
+        {
+            return vector.X * vector1.X + vector.Y * vector1.Y + vector.Z * vector1.Z;
+        }
+
+        public Vector CrossProduct(Vector vector, Vector vector1)
+        {
+            return new Vector
+            {
+                X = vector.Y * vector1.Z - vector.Z * vector1.Y,
+                Y = vector.Z * vector1.X - vector.X * vector1.Z,
+                Z = vector.X * vector1.Y - vector.Y * vector1.X
+            };
+        }
+
+        public double GetCos(Vector vector, Vector vector1)
+        {
+            return ScalarSumm(vector,vector1)/ vector.GetLenght() * vector1.GetLenght();
+        }
+
+        public Vector GetSumm(Vector vector, Vector vector1)
+        {
+            return new Vector
+            {
+                X = vector.X + vector1.X,
+                Y = vector.Y + vector1.Y,
+                Z = vector.Z + vector1.Z
+            };
+        }
+
+        public Vector GetMinus(Vector vector, Vector vector1)
+        {
+
+            return new Vector
+            {
+                X = vector.X - vector1.X,
+                Y = vector.Y - vector1.Y,
+                Z = vector.Z - vector1.Z
+            };
+
+        }
+        public override string ToString()
+        {
+            return string.Format("({0},{1},{2})", X, Y, Z);
+        }
+    }
+}
